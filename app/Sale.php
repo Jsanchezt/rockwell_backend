91dbe02791b386af4ef176ec4562cda7874fbdf5
txp <j.sanchezt@traxion.global>
2023-12-11 23:53:59 -0600

    a

--- app/Sale.php
+++ app/Sale.php
@@ -9,7 +9,7 @@ class Sale extends Model
 
     protected $table = 'sales';
 
-    protected $fillable = ['name', 'email', 'phone', 'address', 'products', 'status','total', 'user_id'];
+    protected $fillable = ['name', 'email', 'phone', 'address', 'products', 'status','total', 'user_id', 'shipment', 'comments'];
 
     protected $casts =[
         'products' => 'array',
