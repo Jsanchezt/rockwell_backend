1d9099960440b92ad5662a34f285a8d06eb903ee
txp <j.sanchezt@traxion.global>
2023-12-20 01:15:40 -0600

    fix

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+
+
 Route::get('/', function () {
     response()->json([
         'api'=> 'traxporta'
@@ -23,6 +29,26 @@ Route::get('/password/reset/{token}',  function ($token) {
 });
 
 
-Route::post('/password/update}',  function () {
-    return "ok";
+Route::post('/password/update}',  function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required|min:8|confirmed',
+        'token' => 'required',
+    ]);
+
+    $status = Password::reset(
+        $request->only('email', 'password', 'password_confirmation', 'token'),
+        function ($user, $password) {
+            $user->forceFill([
+                'password' => Hash::make($password)
+            ])->save();
+
+            Auth::login($user);
+        }
+    );
+
+    // Redirigir según el estado del restablecimiento
+    return $status == Password::PASSWORD_RESET
+        ? redirect()->away('https://www.rockwell.com.mx/sing_in')->with('status', __($status))
+        : back()->withErrors(['email' => [__($status)]]);
 })->name('password.update');
